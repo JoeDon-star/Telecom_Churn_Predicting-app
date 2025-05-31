92b5630e3c46eb48511ceb0b6ff3f984ffadb288
JoeDon-star <nwadikejoseph01@gmail.com>
2025-05-31 16:27:05 +0100

    Update appST.py

--- appST.py
+++ appST.py
@@ -9,7 +9,7 @@ import plotly.express as px
 # Load the model
 @st.cache_resource
 def load_model():
-    with open("Best_model.pkl", "rb") as file:
+    with open("best_model.pkl", "rb") as file:
         return joblib.load(file)
 
 model = load_model()
